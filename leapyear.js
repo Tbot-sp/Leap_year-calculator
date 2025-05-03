78cf9600f39c0ee50dccec65b6c952193a10cae0
Tbot-sp <tobyluk88@gmail.com>
2025-05-03 01:45:33 +0000

    Initial commit of leap year calculator project

--- leapyear.js
+++ leapyear.js
@@ -1,13 +1,11 @@
 // Write code below 💖
 let i = 2000;
 
-while (i<3000) {
+while (i<=3000) {
 
-  if (i%4==0 && i%100!= 0){
-    console.log(i)}
+  if ((i%4==0 && i%100!= 0) || i%400 ==0){
+    console.log(i + ' is a leap year')};
   
-  else if (i%400 ==0){
-    console.log(i)}
-  i = i+1
+    i = i+1
    
 }
