f35429b477ac95aa1aba0b076004c12d3213cb77
Tbot-sp <tobyluk88@gmail.com>
2025-05-03 01:33:37 +0000

    Initial commit of leap year calculator project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Leap_year-calculator
+## 用来输出2000到3000年之间的闰年呢
---
+++ leapyear.js
@@ -0,0 +1,13 @@
+// Write code below 💖
+let i = 2000;
+
+while (i<3000) {
+
+  if (i%4==0 && i%100!= 0){
+    console.log(i)}
+  
+  else if (i%400 ==0){
+    console.log(i)}
+  i = i+1
+   
+}
